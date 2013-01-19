253b1ba3cc9ca531a0b04f7bbdc419efebb97541
Халиков Руслан <khalikoff@gmail.com>
2013-01-19 20:47:51 +0400

    authentication by email+password without registration

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,19 @@
 class SessionsController < Devise::SessionsController
   def create
     puts '========================================'
-    self.resource = warden.authenticate?(auth_options)
+    user_authed = warden.authenticate?(auth_options)
+    puts params
+    user = User.find(:first, :conditions => [ "email = ?", params['user']['email']])
     puts '++++++++++++++++++++++++++++++++++++++++'
     puts self.resource
-    set_flash_message(:notice, :signed_in) if is_navigational_format?
-    if resource
+    if user_authed or user  # Пользователь найден
+      self.resource = warden.authenticate!(auth_options)
       sign_in(resource_name, resource)
-    else
+    else  # новый пользователь - регистрируем
       build_resource
       sign_in(resource_name, resource)
     end
+    set_flash_message(:notice, :signed_in) if is_navigational_format?
     respond_with resource, :location => after_sign_in_path_for(resource)
   end
 end
